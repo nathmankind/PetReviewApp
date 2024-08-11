b17fb59bbebdff93004c08df2a965ec25fda9f2f
nathmankind <nathmankind@gmail.com>
2024-08-11 16:47:37 -0400

    Add project files.

---
+++ PetReviewApp/Data/DataContext.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PetReviewApp.Models;
+
+namespace PetReviewApp.Data
+{
+    public class DataContext: DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options): base(options) 
+        { 
+        }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Review> Reviews { get; set; }
+        public DbSet<Owner> Owners { get; set; }
+        public DbSet<Pokemon> Pokemon { get; set; }
+        public DbSet<Country> Countries { get; set; }
+        public DbSet<PokemonOwner> PokemonOwners { get; set; }
+        public DbSet<PokemonCategory> PokemonCategories { get; set; }
+        public DbSet<Reviewer> Reviewers { get; set; }
+
+       // Linking FKs and Pks with their respective entities
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // This is telling entity framework to link the two IDs pokemonId, categoryId together
+            modelBuilder.Entity<PokemonCategory>()
+                .HasKey(c => new { c.PokemonId, c.CategoryId });
+            modelBuilder.Entity<PokemonCategory>()
+                .HasOne(p => p.Pokemon)
+                .WithMany(pc => pc.PokemonCategories)
+                .HasForeignKey(c => c.PokemonId);
+            modelBuilder.Entity<PokemonCategory>()
+                .HasOne(p => p.Catgory)
+                .WithMany(pc => pc.PokemonCategories)
+                .HasForeignKey(c => c.CategoryId);
+
+            modelBuilder.Entity<PokemonOwner>()
+             .HasKey(po => new { po.PokemonId, po.OwnerId });
+            modelBuilder.Entity<PokemonOwner>()
+                .HasOne(p => p.Pokemon)
+                .WithMany(pc => pc.PokemonOwners)
+                .HasForeignKey(c => c.PokemonId);
+            modelBuilder.Entity<PokemonOwner>()
+                .HasOne(p => p.Owner)
+                .WithMany(pc => pc.PokemonOwners)
+                .HasForeignKey(c => c.OwnerId);
+        }
+    }
+
+}
---
+++ PetReviewApp/Models/PokemonCategory.cs
@@ -0,0 +1,11 @@
+﻿namespace PetReviewApp.Models
+{
+    public class PokemonCategory
+    {
+        public int PokemonId { get; set; }
+
+        public int CategoryId { get; set; }
+        public Pokemon Pokemon { get; set; }
+        public Category Catgory { get; set; }
+    }
+}
